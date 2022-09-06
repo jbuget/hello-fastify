7c26d651e421d1f3fcd01189412d6e34593b4fa5
jbuget <jbuget@gmail.com>
2022-09-06 18:37:03 +0200

    tech: change default server port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Fastify from 'fastify';
 dotenv.config();
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 const fastify = Fastify({
   logger: true
